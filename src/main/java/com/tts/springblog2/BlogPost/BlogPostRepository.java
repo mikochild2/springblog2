02afece6c871965842018e940306f218486e8187
mikochild2 <mikochild2@gmail.com>
2020-06-24 08:38:29 -0400

    started over

---
+++ src/main/java/com/tts/springblog2/BlogPost/BlogPostRepository.java
@@ -0,0 +1,7 @@
+package com.tts.springblog2.BlogPost;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface BlogPostRepository extends CrudRepository<BlogPost, Long> {
+
+}
